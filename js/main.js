19435ddc1ff1a101ee4a2c0f14d1b13d10108e9c
samuelNe-Dev <negash.samuel@yahoo.de>
2021-03-10 13:19:28 +0100

    Added hamburgermenu with menu. Animation has to be adjusted or recreated.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('Hallo');
+const menuBtn = document.querySelector('.hamburger');
+const menuCtn = document.querySelector('.menu-container');
+
+
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuCtn.classList.add('active');
+        menuOpen = true;
+    }else{
+        menuBtn.classList.remove('open');
+        menuCtn.classList.remove('active');
+        menuOpen = false;
+    }
+});
